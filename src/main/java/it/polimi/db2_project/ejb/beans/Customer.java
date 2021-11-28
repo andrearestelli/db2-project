b01a97dc574020701fe79e8ca414ac6214eb0755
domenicoputignano <domenico1.putignano@mail.polimi.it>
2021-11-28 09:55:49 +0100

    added Customer services

--- src/main/java/it/polimi/db2_project/ejb/beans/Customer.java
+++ src/main/java/it/polimi/db2_project/ejb/beans/Customer.java
@@ -1,13 +1,24 @@
 package it.polimi.db2_project.ejb.beans;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.io.Serializable;
 import java.util.List;
 
 @Entity
+@NamedQueries(
+        {
+        @NamedQuery(name = "Customer.checkCredentials"
+                        ,query = "Select c " +
+                        "FROM Customer c " +
+                        "WHERE c.username = :username AND c.password = :password")
+        ,
+        @NamedQuery(name ="Customer.findByUsername"
+                    ,query = "Select c " +
+                            "FROM Customer c " +
+                            "WHERE c.username = :username")
+        }
+)
+
 @Table(name = "customer", schema = "telcodb")
 public class Customer implements Serializable {
     @Id
@@ -15,6 +26,13 @@ public class Customer implements Serializable {
     private String password;
     private String email;
     private boolean insolvent;
+
+    public Customer(String username, String password, String email) {
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.insolvent = false;
+    }
 
     @OneToMany(mappedBy = "userOrderer")
     private List<Order> orders;
@@ -25,9 +43,15 @@ public class Customer implements Serializable {
     @OneToMany(mappedBy = "customer")
     private List<ServiceActivationSchedule> serviceActivationScheduleList;
 
+    public Customer() {
+
+    }
+
     public void setUsername(String username) {
         this.username = username;
     }
+
+    public void setInsolvent(boolean insolvent) {this.insolvent = insolvent;}
 
     public String getUsername() {
         return username;
---
+++ src/main/java/it/polimi/db2_project/ejb/services/CustomerService.java
@@ -0,0 +1,45 @@
+package it.polimi.db2_project.ejb.services;
+
+import it.polimi.db2_project.ejb.beans.Customer;
+
+import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.Optional;
+
+@Stateless
+public class CustomerService {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public Optional<Customer> checkCredentials(String username, String password) {
+        return em.createNamedQuery("Customer.checkCredentials",Customer.class).
+                setParameter("username",username).setParameter("password",password).
+                getResultStream().findFirst();
+    }
+
+    public void registerCustomer(String username, String password, String email) {
+        Customer customer = new Customer(username,password,email);
+        em.persist(customer);
+        em.flush();//TODO comments
+    }
+
+    public Optional<Customer> findCustomerByUsername(String username) {
+        return em.createNamedQuery("Customer.findByUsername", Customer.class)
+                .setParameter("username", username)
+                .getResultStream().findFirst();
+    }
+
+    public void setInsolventTrue(String username) {
+        Optional customer = findCustomerByUsername(username);
+        if(customer.isPresent()) ((Customer) customer.get()).setInsolvent(true);
+        //em.persist(customer);
+    }
+
+    public void setInsolventFalse(String username) {
+        Optional customer = findCustomerByUsername(username);
+        if(customer.isPresent()) ((Customer) customer.get()).setInsolvent(false);
+        //em.persist(customer);
+    }
+}
