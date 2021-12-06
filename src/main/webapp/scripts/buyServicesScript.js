a4ea0b71f0a05d8b28591986e6c23fa2cb12e842
Andrea Restelli <andrearestelli28@gmail.com>
2021-12-06 10:08:10 +0100

    buy services interaction fix

---
+++ src/main/webapp/scripts/buyServicesScript.js
@@ -0,0 +1,30 @@
+function sendOptionalProducts(trigger){
+    var form = trigger.closest("form");
+    if(form.checkValidity()) {
+        var optionalProductList = Array.prototype.slice.call(form.querySelectorAll("input[name='optionalProductID']:checked"))
+            .map(function (x){
+                return x.value;
+            });
+        var servicePackageID = form.querySelector("input[name = 'servicePackageSelected']:checked").value;
+        var subscriptionDate = form.querySelector("input[name = 'subscriptionDate']").value;
+        form = {
+            "servicePackageSelected" : servicePackageID,
+            "optionalProductID" : optionalProductList,
+            "subscriptionDate" : subscriptionDate
+        }
+        form = JSON.stringify(form);
+        var req = new XMLHttpRequest();
+        req.open("POST", "GoToBuyServices");
+        req.setRequestHeader("Content-Type", "application/json");
+        req.send(form);
+    }
+}
+
+function enableCheckboxes(selected){
+    document.querySelectorAll("input[type = 'checkbox']").forEach(value => value.checked = false);
+    document.querySelectorAll("input[type = 'checkbox']").forEach(value => value.disabled = true);
+    console.log(document.querySelectorAll("input[type = 'checkbox']"))
+    var tableRow = selected.closest("tr");
+    console.log(tableRow);
+    tableRow.querySelectorAll("input[type = 'checkbox']").forEach(value => value.disabled = false);
+}
