7397eeb54dd1774aa1b6ad3957f9a1fe9ae5a51e
andrearestelli <andrearestelli28@gmail.com>
2021-12-10 09:00:32 +0100

    paymentResultPage update

--- src/main/java/it/polimi/db2_project/web/controllers/GoToServicePayment.java
+++ src/main/java/it/polimi/db2_project/web/controllers/GoToServicePayment.java
@@ -14,6 +14,7 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Collections;
 import java.util.Random;
 
 @WebServlet("/GoToServicePayment")
@@ -43,9 +44,15 @@ public class GoToServicePayment extends AbstractThymeleafServlet{
                     orderService.findByID(orderID));
             //TODO inviare la lista degli optional products selezionati
         }
+
+        // Update state of order in db
         orderService.setStateByID(orderID, stateType);
+
+        // Remove unconfirmed order from the session
         request.getSession().removeAttribute("unconfirmedOrder");
-        processTemplate(request,response);
+
+        // Append to the template a boolean variable, true if the payment has been accepted, false otherwise
+        processTemplate(request,response, Collections.singletonMap("orderOutcome", stateType == Order.StateType.VALID));
     }
 
     @Override
