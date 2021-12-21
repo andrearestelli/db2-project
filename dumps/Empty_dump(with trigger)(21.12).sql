b80682b09900f534c1185f7a38dc8c18f996bee9
domenicoputignano <domenico1.putignano@mail.polimi.it>
2021-12-21 16:32:25 +0100

    added Empty dump with triggers 21.12

--- dumps/Empty_dump(with trigger)(21.12).sql
+++ dumps/Empty_dump(with trigger)(21.12).sql
@@ -46,6 +46,54 @@ LOCK TABLES `alert` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `average_number_of_optional_products`
+--
+
+DROP TABLE IF EXISTS `average_number_of_optional_products`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `average_number_of_optional_products` (
+  `packageName` varchar(45) NOT NULL,
+  `number_of_sales_package` int DEFAULT NULL,
+  `number_of_opt_products` int DEFAULT NULL,
+  `average_of_opt_products` decimal(7,2) DEFAULT NULL,
+  PRIMARY KEY (`packageName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `average_number_of_optional_products`
+--
+
+LOCK TABLES `average_number_of_optional_products` WRITE;
+/*!40000 ALTER TABLE `average_number_of_optional_products` DISABLE KEYS */;
+/*!40000 ALTER TABLE `average_number_of_optional_products` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `best_seller_optional_product`
+--
+
+DROP TABLE IF EXISTS `best_seller_optional_product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `best_seller_optional_product` (
+  `optionalProduct` varchar(45) NOT NULL,
+  `total_value` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`optionalProduct`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `best_seller_optional_product`
+--
+
+LOCK TABLES `best_seller_optional_product` WRITE;
+/*!40000 ALTER TABLE `best_seller_optional_product` DISABLE KEYS */;
+/*!40000 ALTER TABLE `best_seller_optional_product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `customer`
 --
 
@@ -160,7 +208,7 @@ CREATE TABLE `optional_product` (
   `validity_period` int NOT NULL,
   `monthly_fee` decimal(2,0) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -169,9 +217,29 @@ CREATE TABLE `optional_product` (
 
 LOCK TABLES `optional_product` WRITE;
 /*!40000 ALTER TABLE `optional_product` DISABLE KEYS */;
-INSERT INTO `optional_product` VALUES (1,'Decoder',3,13),(2,'Google news feed',3,8),(3,'Internet Tv channel',2,15),(4,'SMS news feed',3,10),(5,'Youtube Premium',3,5);
+INSERT INTO `optional_product` VALUES (1,'Decoder',3,13),(2,'Google news feed',3,8),(3,'Internet Tv channel',2,15),(4,'SMS news feed',3,10),(5,'Youtube Premium',3,5),(6,'Netflix',4,15),(7,'Spotify',3,23),(8,'DAZN',2,30),(9,'Sky go',3,35);
 /*!40000 ALTER TABLE `optional_product` ENABLE KEYS */;
 UNLOCK TABLES;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `CreateOptional_product_best_seller` AFTER INSERT ON `optional_product` FOR EACH ROW BEGIN
+	IF(new.name NOT IN (SELECT bs.optionalProduct FROM telcodb.best_seller_optional_product bs))THEN
+		INSERT INTO telcodb.best_seller_optional_product 
+		VALUES(new.name,0);
+    END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `order`
@@ -279,6 +347,97 @@ DELIMITER ;
 /*!50003 SET character_set_client  = @saved_cs_client */ ;
 /*!50003 SET character_set_results = @saved_cs_results */ ;
 /*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `UpdateTotalValueServicePackages` AFTER UPDATE ON `order` FOR EACH ROW BEGIN
+	IF((old.state = 'PENDING' OR old.state = 'REJECTED')
+		AND new.state = 'VALID') THEN
+		UPDATE telcodb.total_value_of_sales
+			SET total_value_with_optp = total_value_with_optp + new.total_value
+			WHERE packageName = (SELECT sp.name FROM
+								telcodb.service_package sp JOIN telcodb.order o
+                                ON o.id_package = sp.ID
+                                WHERE o.ID = new.ID);
+		UPDATE telcodb.total_value_of_sales
+			SET total_value_without_optp = total_value_without_optp + (SELECT sp1.monthly_fee*sp1.validity_period FROM
+																		telcodb.service_package sp1 JOIN telcodb.order o1
+																		ON o1.id_package = sp1.ID
+																		WHERE o1.ID = new.ID )
+			WHERE packageName = (SELECT sp.name FROM
+								telcodb.service_package sp JOIN telcodb.order o
+                                ON o.id_package = sp.ID
+                                WHERE o.ID = new.ID);
+	END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `UpdateAverageOptProducts` AFTER UPDATE ON `order` FOR EACH ROW BEGIN
+		IF((old.state = 'PENDING' OR old.state = 'REJECTED')
+		AND new.state = 'VALID') THEN
+		UPDATE telcodb.average_number_of_optional_products
+			SET number_of_sales_package = number_of_sales_package + 1,
+				number_of_opt_products = number_of_opt_products + (SELECT COUNT(*) 
+																	FROM telcodb.order_opt_product_link optl
+                                                                    WHERE optl.IDorder = new.ID),
+				average_of_opt_products = number_of_opt_products/number_of_sales_package
+			WHERE packageName = (SELECT sp.name FROM
+								telcodb.service_package sp JOIN telcodb.order o
+                                ON o.id_package = sp.ID
+                                WHERE o.ID = new.ID);
+		END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_0900_ai_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'STRICT_TRANS_TABLES,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+/*!50003 CREATE*/ /*!50017 DEFINER=`root`@`localhost`*/ /*!50003 TRIGGER `UpdateBestSellerOptProduct` AFTER UPDATE ON `order` FOR EACH ROW BEGIN
+	IF((old.state = 'PENDING' OR old.state = 'REJECTED')
+		AND new.state = 'VALID') THEN
+		UPDATE telcodb.best_seller_optional_product
+        SET total_value = total_value + (SELECT optProd.monthly_fee * optProd.validity_period 
+										FROM telcodb.order_opt_product_link optLink JOIN telcodb.optional_product optProd
+										ON optProd.ID = optLink.ID_optional_product
+                                        WHERE optLink.IDorder = new.ID AND optProd.name = optionalProduct)
+		WHERE optionalProduct IN (SELECT op.name FROM
+								telcodb.optional_product op JOIN telcodb.order_opt_product_link o
+                                ON o.id_optional_product = op.ID
+                                WHERE o.IDorder = new.ID);
+		END IF;
+END */;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
 
 --
 -- Table structure for table `order_opt_product_link`
@@ -329,7 +488,7 @@ CREATE TABLE `package_opt_product_link` (
 
 LOCK TABLES `package_opt_product_link` WRITE;
 /*!40000 ALTER TABLE `package_opt_product_link` DISABLE KEYS */;
-INSERT INTO `package_opt_product_link` VALUES (2,1),(10,1),(2,2),(6,2),(12,2),(1,3),(7,3),(2,4),(4,4),(6,4),(4,5),(10,5);
+INSERT INTO `package_opt_product_link` VALUES (2,1),(10,1),(13,1),(16,1),(2,2),(6,2),(12,2),(13,2),(1,3),(7,3),(14,3),(17,3),(2,4),(4,4),(6,4),(4,5),(10,5),(13,5),(15,6),(16,7),(17,8),(16,9);
 /*!40000 ALTER TABLE `package_opt_product_link` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -431,7 +590,7 @@ CREATE TABLE `service_package` (
   `validity_period` int NOT NULL,
   `monthly_fee` decimal(2,0) NOT NULL,
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=13 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -440,7 +599,7 @@ CREATE TABLE `service_package` (
 
 LOCK TABLES `service_package` WRITE;
 /*!40000 ALTER TABLE `service_package` DISABLE KEYS */;
-INSERT INTO `service_package` VALUES (1,'service1',2,24),(2,'service2',3,20),(3,'service3',6,25),(4,'service4',3,18),(5,'service5',5,45),(6,'service6',3,41),(7,'service7',2,46),(10,'service8',3,47),(11,'service9',4,40),(12,'service8',3,90);
+INSERT INTO `service_package` VALUES (1,'service1',2,24),(2,'service2',3,20),(3,'service3',6,25),(4,'service4',3,18),(5,'service5',5,45),(6,'service6',3,41),(7,'service7',2,46),(10,'service8',3,47),(11,'service9',4,40),(12,'service8',3,90),(13,'service10',3,33),(14,'service10',2,54),(15,'service10',4,50),(16,'service10',3,46),(17,'service1',2,54);
 /*!40000 ALTER TABLE `service_package` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!50003 SET @saved_cs_client      = @@character_set_client */ ;
@@ -457,10 +616,18 @@ DELIMITER ;;
 		INSERT INTO telcodb.total_purchases_per_package 
 		VALUES(new.name,0);
     END IF;
+    IF(new.name NOT IN (SELECT tvp.packageName FROM telcodb.total_value_of_sales tvp))THEN
+		INSERT INTO telcodb.total_value_of_sales 
+		VALUES(new.name,0,0);
+    END IF;
     IF((new.name,new.validity_period) NOT IN (SELECT tp.packageName,tp.validity_period FROM telcodb.total_purchases_validity_period_per_package tp))THEN
 		INSERT INTO telcodb.total_purchases_validity_period_per_package
 		VALUES(new.name,new.validity_period,0);
 	END IF;	
+    IF(new.name NOT IN (SELECT tp.packageName FROM telcodb.average_number_of_optional_products tp))THEN
+		INSERT INTO telcodb.average_number_of_optional_products
+		VALUES(new.name,0,0,0);
+	END IF;
 END */;;
 DELIMITER ;
 /*!50003 SET sql_mode              = @saved_sql_mode */ ;
@@ -491,7 +658,7 @@ CREATE TABLE `service_to_package_link` (
 
 LOCK TABLES `service_to_package_link` WRITE;
 /*!40000 ALTER TABLE `service_to_package_link` DISABLE KEYS */;
-INSERT INTO `service_to_package_link` VALUES (1,1),(2,1),(3,1),(6,2),(8,2),(10,2),(6,3),(7,3),(8,4),(1,5),(4,5),(6,5),(8,6),(9,6),(10,6),(2,7),(7,7),(3,10),(7,10),(6,11),(7,11),(1,12);
+INSERT INTO `service_to_package_link` VALUES (1,1),(2,1),(3,1),(6,2),(8,2),(10,2),(6,3),(7,3),(8,4),(1,5),(4,5),(6,5),(8,6),(9,6),(10,6),(2,7),(7,7),(3,10),(7,10),(6,11),(7,11),(1,12),(5,13),(6,13),(8,14),(9,14),(7,15),(8,15),(9,16),(10,16),(10,17);
 /*!40000 ALTER TABLE `service_to_package_link` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -543,6 +710,30 @@ LOCK TABLES `total_purchases_validity_period_per_package` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `total_value_of_sales`
+--
+
+DROP TABLE IF EXISTS `total_value_of_sales`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `total_value_of_sales` (
+  `packageName` varchar(45) NOT NULL,
+  `total_value_with_optp` decimal(10,2) DEFAULT NULL,
+  `total_value_without_optp` decimal(10,2) DEFAULT NULL,
+  PRIMARY KEY (`packageName`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `total_value_of_sales`
+--
+
+LOCK TABLES `total_value_of_sales` WRITE;
+/*!40000 ALTER TABLE `total_value_of_sales` DISABLE KEYS */;
+/*!40000 ALTER TABLE `total_value_of_sales` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Dumping routines for database 'telcodb'
 --
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -555,4 +746,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2021-12-17 14:56:21
+-- Dump completed on 2021-12-21 16:29:43
